b0223c3835689ea92284c23366179db5b8dbf15f
Stephmo1984 <147718030+Stephmo1984@users.noreply.github.com>
2024-06-03 12:44:29 +0200

    test_threads

---
+++ thread_exemple_1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex;
+int counter = 0;
+
+void *increment_counter(void *thread_id) {
+    long tid = (long)thread_id;
+    
+    // Verrouiller le mutex avant de modifier le compteur
+    pthread_mutex_lock(&mutex);
+    counter++;
+    printf("Thread %ld incremented counter to %d\n", tid, counter);
+    // Déverrouiller le mutex après avoir modifié le compteur
+    pthread_mutex_unlock(&mutex);
+    
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[5];
+    int rc;
+    long t;
+    
+    // Initialiser le mutex
+    pthread_mutex_init(&mutex, NULL);
+    
+    for (t = 0; t < 5; t++) {
+        printf("Creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, increment_counter, (void *)t);
+        
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+    
+    for (t = 0; t < 5; t++) {
+        pthread_join(threads[t], NULL);
+    }
+    
+    printf("Main thread finished. Final counter value: %d\n", counter);
+    
+    // Détruire le mutex
+    pthread_mutex_destroy(&mutex);
+    pthread_exit(NULL);
+}
---
+++ thread_exemple_2.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define NUM_PHILOSOPHERS 5
+
+pthread_mutex_t forks[NUM_PHILOSOPHERS];
+
+void* philosopher(void* num) {
+    int id = *(int*)num;
+    int left = id;
+    int right = (id + 1) % NUM_PHILOSOPHERS;
+
+    while (1) {
+        printf("Philosopher %d is thinking.\n", id);
+        sleep(rand() % 2);
+
+        printf("Philosopher %d is hungry.\n", id);
+
+        // Pick up left fork
+        pthread_mutex_lock(&forks[left]);
+        printf("Philosopher %d picked up left fork %d.\n", id, left);
+
+        // Pick up right fork
+        pthread_mutex_lock(&forks[right]);
+        printf("Philosopher %d picked up right fork %d.\n", id, right);
+
+        // Eat
+        printf("Philosopher %d is eating.\n", id);
+        sleep(rand() % 2);
+
+        // Put down right fork
+        pthread_mutex_unlock(&forks[right]);
+        printf("Philosopher %d put down right fork %d.\n", id, right);
+
+        // Put down left fork
+        pthread_mutex_unlock(&forks[left]);
+        printf("Philosopher %d put down left fork %d.\n", id, left);
+    }
+}
+
+int main() {
+    pthread_t philosophers[NUM_PHILOSOPHERS];
+    int ids[NUM_PHILOSOPHERS];
+    int i;
+
+    // Initialize mutexes
+    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_mutex_init(&forks[i], NULL);
+    }
+
+    // Create philosopher threads
+    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+        ids[i] = i;
+        pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
+    }
+
+    // Join philosopher threads (in practice, this will not be reached)
+    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_join(philosophers[i], NULL);
+    }
+
+    // Destroy mutexes (in practice, this will not be reached)
+    for (i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_mutex_destroy(&forks[i]);
+    }
+
+    return 0;
+}
