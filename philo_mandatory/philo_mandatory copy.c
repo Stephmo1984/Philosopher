42e94b0652b905604b51f6b31a42c2eaa0b4cf65
Stephanie Mortemousque <smortemo@3D1.42angouleme.fr>
2024-07-02 16:22:08 +0200

    added timestamp

--- philo_mandatory/philo_mandatory copy.c
+++ philo_mandatory/philo_mandatory copy.c
@@ -24,18 +24,6 @@ unsigned long	get_time_millisec(void)
 	return (time_usec / 1000);
 }
 
-unsigned long	get_timestamp_millisec(unsigned long start_milli)
-{
-	struct timeval time;
-	unsigned long time_millisec;
-	
-	gettimeofday(&time, NULL);
-	
-	time_millisec = ((time.tv_sec * 1000000) + time.tv_usec) / 1000;
-
-	return (time_millisec - start_milli);
-}
-
 
 
 void	print_philo(t_philo_thread *thread_n, int n, pthread_mutex_t *forks)
@@ -73,89 +61,103 @@ void	*philo_do(void *thread_philo_n)
 	int time_eat = thread_n->data->t_eat;
 	int time_sleep = thread_n->data->t_sleep;
 	int meals = thread_n->data->nbr_meals;
-	unsigned long	start_simu = thread_n->data->start;
 	
 	int l = n - 1;
     int r =  n % thread_n->data->nbr_phi;
 
 	// https://www.delftstack.com/fr/howto/c/gettimeofday-in-c/
-	while(counter_meals != 0) 
-	// while(1)
+	//while(counter_meals != 0) 
+	while(1)
 	{
 		if (n % 2 == 0)
 		{
 			pthread_mutex_lock(&thread_n->data->mtx_print);
 			write(1,"--------------\n", 15);
+			if(counter_meals == thread_n->data->nbr_meals)
+				printf("PHILO %d ***TIME**** LAST_MEAL_INIT = %li\n --------------\n", n, thread_n->last_meal);
+			printf("Philosopher %d is thinking.\n", n);
+			
 			pthread_mutex_lock(&forks[l]);
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d has taken L fork (%i).\n", n, n);
+			printf("Philosopher %d picked L fork (%i) %p.\n", n, n, &forks[l]);
+
 			pthread_mutex_lock(&forks[r]);
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d has taken R fork (%i).\n", n, n-1);
+			printf("Philosopher %d picked R fork (%i) %p.\n", n, n-1, &forks[r]);
+			
 			thread_n->start_meal = get_time_millisec();
 			printf("***TIME PHILO_START_MEAL from last meal****  = %li\n", thread_n->start_meal - thread_n->last_meal);
 			if ((thread_n->start_meal - thread_n->last_meal) >= thread_n->data->t_death)
-			// if ((get_timestamp_millisec(thread_n->last_meal)) >= thread_n->data->t_death)
 			{
-
 				printf("XXXXXXXXXXXXXXXXXX PHILO %d *** XXXX DEAD XXXXX **** \n", n);
+
 				exit(-1);
 			}
-			printf("P%d is eating.\n", n);	
+			printf("Philosopher %d is eating.\n", n);	
+
 			usleep(time_eat);
 			thread_n->last_meal = thread_n->start_meal;
+			
 			pthread_mutex_unlock(&forks[r]);
+			printf("Philosopher %d put down R fork (%i) %p.\n", n, n - 1, &forks[r]);
+
 			pthread_mutex_unlock(&forks[l]);
+			printf("Philosopher %d put down L fork (%i) %p.\n", n, n, &forks[l]);
+			
 			counter_meals--;
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d is sleeping.\n", n);
+
+			printf("Philosopher %d is sleeping.\n", n);
 			usleep(time_sleep);
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d is thinking.\n", n);
+
 			pthread_mutex_unlock(&thread_n->data->mtx_print);
 		}
 		else if (n % 2 != 0)
 		{
 			pthread_mutex_lock(&thread_n->data->mtx_print);
 			write(1,"--------------\n", 15);
+			if(counter_meals == thread_n->data->nbr_meals)
+				printf("/////////////// PHILO %d ***TIME**** LAST_MEAL_INIT = %li\n --------------\n", n, thread_n->last_meal);
+
+			printf("Philosopher %d is thinking.\n", n);
+			
 			pthread_mutex_lock(&forks[r]);
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d has taken R fork (%i).\n", n, n-1);
+			printf("Philosopher %d picked R fork (%i) %p.\n", n, n-1, &forks[r]);
+			
 			pthread_mutex_lock(&forks[l]);
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d has taken L fork (%i).\n", n, n);
+			printf("Philosopher %d picked L fork (%i) %p.\n", n, n, &forks[l]);
+						
 			thread_n->start_meal = get_time_millisec();
 			printf("***TIME PHILO_START_MEAL from last meal**** = %li\n", thread_n->start_meal - thread_n->last_meal);
-
 			if ((thread_n->start_meal - thread_n->last_meal) >= thread_n->data->t_death)
-			// if ((get_timestamp_millisec(thread_n->last_meal)) >= thread_n->data->t_death)
 			{
 				printf("XXXXXXXXXXXXXXXXXX PHILO %d *** XXXX DEAD XXXXX **** \n", n);
 				exit (-1);
 			}
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d is eating.\n", n);	
+			printf("Philosopher %d is eating.\n", n);	
 			usleep(time_eat);
 			thread_n->last_meal = thread_n->start_meal;
+			
 			pthread_mutex_unlock(&forks[l]);
+			printf("Philosopher %d put down L fork (%i) %p.\n", n, n, &forks[l]);
+			
 			pthread_mutex_unlock(&forks[r]);
+			printf("Philosopher %d put down R fork (%i) %p.\n", n, n - 1, &forks[r]);
+
 			counter_meals--;
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d is sleeping.\n", n);
+
+			printf("Philosopher %d is sleeping.\n", n);
 			usleep(time_sleep);
-			printf("[%li]  ", get_timestamp_millisec(start_simu));
-			printf("P%d is thinking.\n", n);
+
 			pthread_mutex_unlock(&thread_n->data->mtx_print);
 		}
 	}
-	return (NULL);
+	pthread_exit(NULL); //???
+	// exit (1);
 }
 
 
 void init_data(t_data *data)
 {
 	data->nbr_phi = 5;
-	data->t_death = 1000;
+	data->t_death = 102;
 	data->t_eat= 200;
 	data->t_sleep = 200;
 	data->nbr_meals = 7;
